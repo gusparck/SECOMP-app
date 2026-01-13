565cf2aca1f06bf092c703202ea8f2d7df86d68f
PHdaCruzSantos <phdacruzsantos.dev@gmail.com>
2026-01-13 18:41:36 -0300

    feat: flutter initial setup and first test  pages

---
+++ frontend/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'core/theme/app_theme.dart';
+import 'features/auth/login_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SECOMP',
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ frontend/lib/features/auth/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(decoration: const InputDecoration(labelText: 'Email')),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Senha'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: const Text('Entrar')),
+          ],
+        ),
+      ),
+    );
+  }
+}
