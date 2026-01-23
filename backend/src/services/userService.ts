efd8a48d66b9aac78a3249a1dd8f973a0d0283e4
gusparck <dabx.yta@gmail.com>
2026-01-23 17:09:56 -0300

    feat: UserCreation / update no funcionamento do controller e do service, com integração do 'zod'

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -7,40 +7,26 @@ interface CreateUserInput {
     email: string;
     password: string;
     registration: string; //matrícula
-    course?: string;
-    campus?: string;
+    course?: string | null | undefined;
+    campus?: string | null | undefined;
 }
 
 export class UserService {
-    // Criando usuário 
     async createUser(data: CreateUserInput) {
+
         const existingEmail = await prisma.user.findUnique({
             where: { email: data.email }
-        })
+        });
         if (existingEmail) {
-            throw new Error("E-mail já registrado no sistema.")
+            throw new Error("CONFLICT");
         }
 
         const existingRegistration = await prisma.user.findUnique({
             where: { registration: data.registration }
-        })
+        });
         if (existingRegistration) {
-            throw new Error("Número de matrícula inválido ou já utilizado.")
+            throw new Error("CONFLICT");
         }
-
-        const defaultPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
-
-        if (!defaultPassword.test(data.password)) {
-            // Nota: Seria melhor realizar as verficações da estrutura de senha de forma mais ramificada?
-            throw new Error("Senha inválida! Ela precisa ter no mínimo 8 caracteres, um número e um caracter especial (!@#$%^&*).");
-        }
-
-
-        /*const coursesICEA = ["Sistemas de Informação", "Engenharia Elétrica", "Engenharia da Computação", "Engenharia de Produção"];
-
-        if (data.course && !coursesICEA.includes(data.course)) {
-            throw new Error("Curso inválido. Preencha o campo com um curso do campus ICEA.")
-        }*/
 
 
         const createdUser = await prisma.user.create({
